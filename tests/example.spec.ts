612a9f3b3664ecc2833656e2b4d185705b3bc6a1
Jayesh Choudhary <jayeshchoudhary619@gmail.com>
2024-09-26 19:18:24 +0530

    update test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -28,7 +28,7 @@ test("login to admin portal dev", async ({ page }) => {
     await page.getByLabel("Password").fill(process.env.PASSWORD || "test");
     await page.getByRole("button", { name: "LOG IN", exact: true }).click();
 
-    await page.waitForURL("/overview");
+    await page.waitForURL("https//admin-portal.startree-dev.cloud/overview");
 
     await expect(page.getByRole("heading", { name: "Overview" })).toBeVisible();
 });
